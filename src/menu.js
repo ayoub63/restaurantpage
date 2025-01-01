b8ef7646b9b69b12f9dfa9ce83785a07d81a470e
ayoub63 <ayoubafkir18@gmail.com>
2025-01-01 19:16:24 +0100

    menu complete

--- src/menu.js
+++ src/menu.js
@@ -11,82 +11,90 @@ export function menu () {
     const div = document.getElementById("content")
 
     const html = `
+    <div class="menu-container">
 
-          <div class="menu-div">
+
+    <h1> KFC - Kentucky Menu</h1>
+    <p>Availability may differ at different locations</p>
+
+
+
+    <div class="large-item">
+    <img src = "${chicken}" alt="Chicken Burger">
+
+
+    <div class="right-side">
+    <div class="right-top">
+    <h2>Classics</h2>
+    <p>From our original KFC Chicken Sandwich to our KFC® 
+    Nuggets and more, we have something for everyone.</p>
+    <button>View All Classics</button>
+    </div>
     
-          <div id = "largeitem" class="menu-card">
-          <img src="${chicken}" alt="chickbowl" class="menu-img">
-          <div class="lower-menu">
-          <div class="text"><h3>Get Everything Y’all Love Starting At $20</h3>
-          <p>Because nothing beats quality time and quality food. Order your Festive Family Box today!</p></div>
-          
-          <button class="lower-btn">Gimme!</button>     
-          </div>
-          </div>
+    <div class="right-bottom">
+    <div class="right-mini-one">
+    <img src="${nuggets}" alt="" class="mini-img">
+    <div class = "mini-mini">
+    <h3 class="mini-header">Nuggets</h3>
+    <button class="">Order now</button>
+    </div>
+    </div>
 
-          <div class="card-container">
-          <div class="menu-card">
-          <img src="${nuggets}" alt="chickbowl" class="menu-img">
-          <div class="lower-menu">
-         <div class="text"><h3>Get Everything Y’all Love Starting At $20</h3>
-          <p>Because nothing beats quality time and quality food. Order your Festive Family Box today!</p></div>
-          <button class="lower-btn">Gimme!</button>     
-          </div>
-          </div>
+    <div class="right-mini-one">
+    <img src="${spicy}" alt="spicy" class="mini-img">
+    <div class = "mini-mini"> <h3 class="mini-header">Spicy Chicken Burger</h3>
+    <button class="">Order now</button></div>
+   </div>
+    </div>
+    </div>
+
+
+    </div>
+
+    <div class="itemgrid">
+
+    <div class="item">
+    <img src="${salad}" alt="">
+    <h3>Salads</h3>
+    <p>Go green with these options, prepared fresh daily, 
+    and perk them up with our variety of dressings.</p>
+    <button>View All Classics</button>
+    </div>
     
-           <div class="menu-card">
-          <img src="${salad}" alt="chickbowl" class="menu-img">
-          <div class="lower-menu">
-        <div class="text"><h3>Get Everything Y’all Love Starting At $20</h3>
-          <p>Because nothing beats quality time and quality food. Order your Festive Family Box today!</p></div>
-          <button class="lower-btn">Gimme!</button>     
-          </div>
-          </div>
-          </div>
-        
-          
-         <div class="card-container"> 
-        <div class="menu-card">
-        <img src="${shake}" alt="chickbowl" class="menu-img">
-        <div class="lower-menu">
-        <div class="text"><h3>Get Everything Y’all Love Starting At $20</h3>
-        <p>Because nothing beats quality time and quality food. Order your Festive Family Box today!</p>
-        <button class="lower-btn">Gimme!</button>     
-</div>
-        </div>
-        </div>
     
-        <div class="menu-card">
-        <img src="${sides}" alt="chickbowl" class="menu-img">
-        <div class="lower-menu">
-        <div class="text"><h3>Get Everything Y’all Love Starting At $20</h3>
-        <p>Because nothing beats quality time and quality food. Order your Festive Family Box today!</p></div>
-        <button class="lower-btn">Gimme!</button>     
-        </div>
-        </div>
-        </div>
+    <div class="item">
+    <img src="${sides}" alt="">
+    <h3>Salads</h3>
+    <p>Go green with these options, prepared fresh daily, 
+    and perk them up with our variety of dressings.</p>
+    <button>View All Classics</button>
+    </div>
+    
+    </div>
 
-         <div class="card-container"> 
-        <div class="menu-card">
-        <img src="${spicy}" alt="chickbowl" class="menu-img">
-        <div class="lower-menu">
-        <div class="text"><h3>Get Everything Y’all Love Starting At $20</h3>
-        <p>Because nothing beats quality time and quality food. Order your Festive Family Box today!</p></div>
-        <button class="lower-btn">Gimme!</button>     
-        </div>
-        </div>
 
-        <div class="menu-card">
-        <img src="${treat}" alt="chickbowl" class="menu-img">
-        <div class="lower-menu">
-        <div class="text"><h3>Get Everything Y’all Love Starting At $20</h3>
-        <p>Because nothing beats quality time and quality food. Order your Festive Family Box today!</p></div>
-        <button class="lower-btn">Gimme!</button>     
-        </div>
-        </div>
-        </div>
-        </div>
 
+    <div class="itemgrid">
+
+    <div class="item">
+    <img src="${shake}" alt="">
+    <h3>Salads</h3>
+    <p>Go green with these options, prepared fresh daily, 
+    and perk them up with our variety of dressings.</p>
+    <button>View All Classics</button>
+    </div>
+    
+    
+    <div class="item">
+    <img src="${treat}" alt="">
+    <h3>Salads</h3>
+    <p>Go green with these options, prepared fresh daily, 
+    and perk them up with our variety of dressings.</p>
+    <button>View All Classics</button>
+    </div>
+    
+    </div>
+     </div>     
 `
     div.innerHTML = html
 }
